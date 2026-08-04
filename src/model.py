429ef9f500f4b48e8cd803b3f3ad7ca492eaaa9f
Zaneyen <yenfaozane03@gmail.com>
2026-08-04 12:48:31 +0200

    Bach Normalisation de model.py

--- src/model.py
+++ src/model.py
@@ -20,39 +20,38 @@ class SimpleCNN(nn.Module):
 
         # Bloc convolutionnel 1
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
 
         # Bloc convolutionnel 2
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
 
-        # Pooling 
+        # Pooling
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.fc1 = nn.Linear(in_features=64 * 7 * 7, out_features=128)
         self.fc2 = nn.Linear(in_features=128, out_features=10)
 
-        # Dropout pour réduire le sur-apprentissage
+        # Dropout ici pour réduire le sur-apprentissage
         self.dropout = nn.Dropout(0.25)
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.pool(x)          
 
-        # A cette étape, la structure devrait être [batch, 32, 14, 14]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.pool(x)          # [batch, 32, 14, 14]
 
         x = self.conv2(x)
+        x = self.bn2(x)
         x = F.relu(x)
-        x = self.pool(x)          
+        x = self.pool(x)          # [batch, 64, 7, 7]
 
-        #Ici, elle est à [batch, 64, 7, 7]
-
-        x = x.view(x.size(0), -1)  
-
-        # Puis enfin, on a Flatten [batch, 3136]
+        x = x.view(x.size(0), -1)  # Flatten -> [batch, 3136]
 
         x = self.dropout(x)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)         
+        x = self.fc2(x)            # Logits bruts, pas de softmax ici
 
         return x
 
